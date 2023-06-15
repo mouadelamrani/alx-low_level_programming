d01a3229d9f3cb55c7b15f45720a686effe6f83f
mouadALX <mouaadamrani@gmail.com>
2023-06-15 13:05:48 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - is the starting point of the program
+ *
+ * Return: equals 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
