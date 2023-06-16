6a225de9734dc9a7379b3c148833446e43257708
mouadALX <mouaadamrani@gmail.com>
2023-06-16 11:11:41 -0700

    fix 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 /**
-  * main is the starting point
+  * main - is the starting point
   * Description: 2-print_alphabet.c
   * Return: is 0
   */
 int main(void)
 {
+
 	int chN;
+
 	for (chN = 97; chN <= 122; chN++)
 	{
 		putchar(chN);
 	}
 	putchar('\n');
+
 	return (0);
 }
