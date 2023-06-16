222784eaeb2e6b7dc4acdfb2f4eabe9ccda8ea7c
mouadALX <mouaadamrani@gmail.com>
2023-06-16 11:30:09 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+  * main - is the starting point
+  * Description: 2-print_alphabet.c
+  * Return: is 0
+  */
+int main(void)
+{
+
+	int chN;
+
+	for (chN = 97; chN <= 122; chN++)
+	{
+		if (chN == 101 || chN == 113)
+			{
+			chN++;
+			continue;
+			}
+		putchar(chN);
+	}
+	putchar('\n');
+
+	return (0);
+}
