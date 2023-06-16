1851035432a64e38ddb0584a490b930fefee59df
mouadALX <mouaadamrani@gmail.com>
2023-06-16 11:38:01 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - starting point of this program
+ * Description: print number from 0 to 10, followed by a new line
+ * Return: is 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
