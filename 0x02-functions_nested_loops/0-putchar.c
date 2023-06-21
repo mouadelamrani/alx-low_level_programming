2ade48af96dc2f453a39aafcc96a5563fa101f69
mouadALX <mouaadamrani@gmail.com>
2023-06-20 17:22:21 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - is the starting point of the program
+ * Description: this prorgram uses a custom function
+ * to print outputs
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is for the 0x02-functions_nested_loops task
