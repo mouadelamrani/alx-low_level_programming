bcfea42014851941464ff2c7fa9aab9562172697
mouadALX <mouaadamrani@gmail.com>
2023-06-20 19:22:56 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer -  The starting point
+ * Description: this function prints time from 00:00 to 23:59
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int h = 0;
+	int m;
+
+	while (h <= 23)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
