47c31d4a2a93293063014966c0405cdd53f06500
mouadALX <mouaadamrani@gmail.com>
2023-06-21 11:40:40 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - The starting point
+ * Description: this function prints 
+ * Return: void
+ */
+
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+
+	return (sum);
+}
