deccaa86ce6c2e18458166162b4ab5a768c4220c
mouadALX <mouaadamrani@gmail.com>
2023-06-22 08:12:46 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - The strating point
+ * Description: This function multiplies a and b
+ * @a: the first number
+ * @b: the second number
+ * Return: the result of a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
