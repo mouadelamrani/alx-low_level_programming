3d91ab903790ebdb733557622b82b1b4922a222c
mouadALX <mouaadamrani@gmail.com>
2023-06-22 09:01:56 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - the starting point
+ * Description: Prints a triangle, using the character #.
+ * @size: The size of the triangle.
+ */
+void print_triangle(int size)
+{
+	int h, i;
+
+	if (size > 0)
+	{
+		for (h = 1; h <= size; h++)
+		{
+			for (i = size - h; i > 0; i--)
+				_putchar(' ');
+
+			for (i = 0; i < h; i++)
+				_putchar('#');
+
+			if (h == size)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
