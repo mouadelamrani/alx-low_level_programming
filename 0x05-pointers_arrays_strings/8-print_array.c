6a1974c3f114fc65b9519569e66333e701f1ca1f
mouadALX <mouaadamrani@gmail.com>
2023-06-26 10:28:20 -0700

    fix 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_array - is the functions prints all elements 
+ * print_array - is the functions prints all elements
  * of an array of integers
  * @a: the array
  * @n: the length of the array
