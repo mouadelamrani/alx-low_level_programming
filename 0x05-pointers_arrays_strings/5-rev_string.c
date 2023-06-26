60cb95707d9d7d1efaf9b43932cea773250c2a61
mouadALX <mouaadamrani@gmail.com>
2023-06-26 10:10:32 -0700

    git add .

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - is the function that reverses strings
+ * @s: the string
+ */
+
+void rev_string(char *s)
+{
+	int i, j, len;
+	char c;
+
+	i, j = 0;
+	while (s[i])
+		i++;
+	len = i - 1;
+	while (j < i / 2)
+	{
+		c = s[j];
+		s[j] = s[len];
+		s[len] = c;
+		len--;
+		j++;
+	}
+}
