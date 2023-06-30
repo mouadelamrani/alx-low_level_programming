a84ed27195225af7a9c4e467dc59189c22ede301
mouadALX <mouaadamrani@gmail.com>
2023-06-30 12:04:22 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - this function converts lowercase characters to uppercase
+ * @s: the string
+ * Return: the converted string
+ **/
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+	while (s[i])
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] += 'A' - 'a';
+		}
+		i++;
+	}
+	return (s);
+}
